9dc8622a27218a85abfa10053b83ab9bf27b0f21
maheeadhikari <144558756+maheeadhikari@users.noreply.github.com>
2024-06-30 21:51:15 +0530

    Add files via upload

---
+++ 02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("3+3")// we are using nodejs not browser
+
+console.log(3
+    +
+    3); // code readability should be high 
+
+
+console.log("Mahee")
+
+let name = "Mahee" // string data type
+let age =18 // number
+let isLoggedIn = true // boolean
+
+//Primitive DataTypes -
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// bolean => true/false 
+// null => standalon value
+// undefined => value is not defined
+// symbol => unique 
+
+// object 
+
+console.log(typeof null);   //shows object as output
+
+console.log(typeof undefined);  
