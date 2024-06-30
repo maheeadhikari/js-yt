18b149e0ac8fb0867e11f8feb70dc443ad4e050b
maheeadhikari <144558756+maheeadhikari@users.noreply.github.com>
2024-06-30 18:35:23 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "adhikariramesh49405@gmail.com"
+var accountPassword = "123321"
+accountCity = "Champawat"
+let accountState;
+
+// accountId = 3 // not allowed
+accountEmail = "ridhs321@gmail.com"
+
+accountPassword - "43234"
+
+accountCity = "Lohaghat"
+
+console.log( accountId);
+
+/*
+Prefer not to use var because of 
+issue in block scope and function scope
+*/
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
