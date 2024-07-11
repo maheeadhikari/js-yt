b878e890e1431d37b078ac12a7ee7006a45fc795
maheeadhikari <144558756+maheeadhikari@users.noreply.github.com>
2024-07-11 15:42:35 +0530

    Add files via upload

---
+++ truthyfalsy.js
@@ -0,0 +1,43 @@
+const userEmail = "mMahee@ai"
+
+if(userEmail){
+    console.log("Got user Email");
+} else{
+    console.log("Don't have user email");
+}
+
+//when it is assumed that the value is true always in the string 
+
+// empty array = true , empty string = false
+
+// falsy values -- false , zero(0), -0 , BigInt 0n, "", null, undefined, NaN
+
+//truthy values -- "0", "false", " ", [], {}, function(){}
+
+//if(userEmail.length ===0 ){
+  //      console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+    if(Object.keys(emptyObj).length === 0){
+        console.log("object is empty");
+}
+// nullish coalescing operator (??): null undefined
+
+
+let val1;
+//val1 = 5 ?? 10 
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+
+console.log(val1);
+
+//terniary Operator -- small index of if else operator 
+ 
+//condition ? true  :  false 
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.loglog("less than 80") : console.log("more than 80")
